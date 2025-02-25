87e7aa12f39b9a8f088c17bc66b0b64118fb68c6
AhmedHamza667 <hamzaa667788@gmail.com>
2025-02-25 18:11:04 -0500

    Initial Commit

---
+++ weatherAppPracticeSwiftUI/weatherAppPracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppPracticeSwiftUIApp.swift
+//  weatherAppPracticeSwiftUI
+//
+//  Created by Ahmed Hamza on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppPracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
